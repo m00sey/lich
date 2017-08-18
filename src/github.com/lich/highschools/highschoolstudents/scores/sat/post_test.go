baccfd14bdf02fee3a5589cbe87a33bdf4545043
m00sey <kg@scoir.com>
2017-08-18 08:32:17 -0400

    tweak

--- src/github.com/lich/highschools/highschoolstudents/scores/sat/post_test.go
+++ src/github.com/lich/highschools/highschoolstudents/scores/sat/post_test.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"log"
 
+	"net/http"
+
+	"strings"
+
 	"github.com/lich/swagger"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -29,7 +33,25 @@ var _ = Describe("", func() {
 		})
 
 		It("", func() {
+
+			body := `{
+				"User":[{
+
+				}]
+			}`
+
+			c := http.Client{}
+			req, err := http.NewRequest(http.MethodPost, "http://local.scoir.com:9001/integration/load",
+				strings.NewReader(body))
+			Expect(err).ToNot(HaveOccurred())
+
+			resp, err := c.Do(req)
+			Expect(err).ToNot(HaveOccurred())
+
+			log.Println(resp)
+
 			sat := swagger.Sat{
+
 				TestDate:     "10/10/2010",
 				MathScore:    11,
 				ReadingScore: 12,
@@ -38,6 +60,7 @@ var _ = Describe("", func() {
 
 			ctx := context.TODO()
 			ctx = context.WithValue(ctx, swagger.ContextAccessToken, "")
+
 			sat, resp, err := api.DefaultApi.HighSchoolHighSchoolIdStudentStudentIdSATPost(ctx, "", "", sat)
 			Expect(err).ShouldNot(HaveOccurred())
 
